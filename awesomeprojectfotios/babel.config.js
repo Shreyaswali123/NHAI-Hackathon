82a9c8939162beb64d97dcfff1163ac957434234
Shreyaswali123 <146411380+Shreyaswali123@users.noreply.github.com>
2026-05-30 21:02:00 +0530

    Delete awesomeprojectfotios directory

--- awesomeprojectfotios/babel.config.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-  presets: ['module:@react-native/babel-preset'],
-  plugins: [
-    // react-native-reanimated/plugin must be listed last
-    'react-native-reanimated/plugin',
-  ],
-};
